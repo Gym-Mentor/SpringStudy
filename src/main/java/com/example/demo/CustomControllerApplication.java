454fb0823c35ec53a90e88312214859a4781cbef
박종한 <jonghan1983@gmail.com>
2024-07-13 14:27:59 +0900

    first init

---
+++ src/main/java/com/example/demo/CustomControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomControllerApplication.class, args);
+	}
+
+}
